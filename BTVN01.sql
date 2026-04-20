b383160fe27438efcf807f02b631c2b42d91c199
HoangDung34 <hngdungtr55@gmail.com>
2026-04-20 15:05:38 +0700

    Update BTVN01.sql

--- BTVN01.sql
+++ BTVN01.sql
@@ -4,6 +4,6 @@ PHÂN TÍCH:
     WHERE DISTRICT = 'QUẬN 1' OR (DISTRICT = 'QUẬN 3' AND RATING > 4.0);
     ==> LẤY TẤT CẢ QUẬN 1 KHÔNG QUAN TÂM ĐẾN RATING CÒN QUẬN 3 MỚI LỌC ĐIỀU KIỆN RATING > 4.0
 */
-SELECT restaurant_name, address, rating
-FROM Restaurants
-WHERE (district = 'Quận 1' OR district = 'Quận 3') AND rating > 4.0;
+SELECT RESTAURANT_NAME, ADDRESS, RATING
+FROM RESTAURANTS
+WHERE (DISTRICT = 'QUẬN 1' OR DISTRICT = 'QUẬN 3') AND RATING > 4.0;
